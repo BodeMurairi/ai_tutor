c2c13705f9623549b3ad6ec22a6efa5cef20ba53
BodeMurairi2 <b.murairi@alustudent.com>
2026-05-18 09:16:59 +0200

    AI tutor creator creator successfully

--- coding_tutor_agent/agent.py
+++ coding_tutor_agent/agent.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+
 from google.adk.agents.llm_agent import Agent
+from google.adk.tools import google_search
+from .prompt.instruction import prompt
 
 root_agent = Agent(
     name='coding_tutor_agent',
     model='gemini-2.5-flash',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description='You are Mr.Brown, a coding tutor with the goal of teaching user programming',
+    instruction=prompt,
+    tools=[google_search]
 )
