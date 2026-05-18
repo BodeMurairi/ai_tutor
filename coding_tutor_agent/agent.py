1019b114aaf77e279cc56c5624864c344f9639d2
BodeMurairi2 <b.murairi@alustudent.com>
2026-05-18 08:51:59 +0200

    Repository structure created

---
+++ coding_tutor_agent/agent.py
@@ -0,0 +1,8 @@
+from google.adk.agents.llm_agent import Agent
+
+root_agent = Agent(
+    name='coding_tutor_agent',
+    model='gemini-2.5-flash',
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge',
+)
